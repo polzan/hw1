e11fdc2428d39d75a6d2d3bd46659c788b25b1fa
Riccardo Zanol <ricc@zanol.eu>
2016-03-28 01:41:55 +0200

    Fix the AR model psd estimation

--- matlab/psd_ar_estim.m
+++ matlab/psd_ar_estim.m
@@ -1,21 +1,22 @@
-function psd = psd_ar_estim(x, N)
+function psd = psd_ar_estim(x, N, f, Fs)
 r_whole = autocorrelation_unbiased(x);
 r_trunc = r_whole(1:N);
-R = toeplitz(r_trunc).';
+R = transpose(toeplitz(r_trunc));
 
-% assert(all(R(:,1) == r_trunc));
-% assert(all(R(:,2) == [conj(r_trunc(2)); r_trunc(1:N-1)]));
-% assert(all(R(:,N) == flip(conj(r_trunc))));
+assert(all(R(:,1) == r_trunc));
+assert(all(R(:,2) == [conj(r_trunc(2)); r_trunc(1:N-1)]));
+assert(all(R(:,N) == flip(conj(r_trunc))));
 
-% fprintf('The determinant of R is %f\n', det(R));
 r = r_whole(2:N+1);
-a = -inv(R) * r;
+%a = -inv(R) * r;
+linsolve_opts.SYM = true;
+a = linsolve(R, -r, linsolve_opts);
 
 sigma2w_estim = r_whole(1) + (r'*a);
-%fprintf('The AR model estimates a noise power of %f\n', sigma2w_estim);
+fprintf('The AR model estimates a noise power of %f\n', sigma2w_estim);
 sigma2w_estim = abs(real(sigma2w_estim));
 
-H = freqz(1, a, 1000, 1, 'whole');
-
+a_tf = [1; a];
+H = freqz(1, a_tf, 'whole', f, Fs);
 psd = sigma2w_estim .* abs(H).^2;
 end
--- matlab/test_ar.m
+++ matlab/test_ar.m
@@ -6,24 +6,28 @@ sigma2w = 1;
 
 r_whole = theoretical_autocorr(sigma2w, N+1);
 r_trunc = r_whole(1:N);
-R = toeplitz(r_trunc).';
+R = transpose(toeplitz(r_trunc));
 
-% assert(all(R(:,1) == r_trunc));
-% assert(all(R(:,2) == [conj(r_trunc(2)); r_trunc(1:N-1)]));
-% assert(all(R(:,N) == flip(conj(r_trunc))));
+assert(all(R(:,1) == r_trunc));
+assert(all(R(:,2) == [conj(r_trunc(2)); r_trunc(1:N-1)]));
+assert(all(R(:,N) == flip(conj(r_trunc))));
 
 % fprintf('The determinant of R is %f\n', det(R));
 r = r_whole(2:N+1);
 %a = -inv(R) * r;
-a = -linsolve(R, r);
+a = linsolve(R, -r);
+
+
 
 sigma2w_estim = r_whole(1) + (r'*a);
-%fprintf('The AR model estimates a noise power of %f\n', sigma2w_estim);
+fprintf('The AR model estimates a noise power of %f\n', sigma2w_estim);
 sigma2w_estim = abs(real(sigma2w_estim));
 
-H = freqz(1, a, 1000, 1, 'whole');
+a_whole = [1; a];
+f = linspace(0, 1, 1000);
+H = freqz(1, a_whole, 'whole', f, 1);
 
 psd = sigma2w_estim .* abs(H).^2;
 
-f = linspace(0, 1, 1000);
+% = linspace(0, 1, 1000);
 plot(f, 10*log10(psd));

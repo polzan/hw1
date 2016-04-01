f5347409f969420ed0f027f7b1fa711b5f615a5d
Riccardo Zanol <ricc@zanol.eu>
2016-04-01 22:19:48 +0200

    Finish part 4 To check: logscale of the MSE plots

---
+++ matlab/lsm_predictor.m
@@ -0,0 +1,15 @@
+function [c, e, y, k_c, k_ey] = lsm_predictor(x, N, mu)
+K = length(x);
+% Skip the first N-1 samples: we need a full vector x_k
+c = zeros(N, K-N+1);
+y = zeros(K-N, 1);
+e = zeros(K-N, 1);
+for k=1:K-N % -1 because we need the last d(k)
+    x_k = flip(x(k : k + N - 1)); % The first should be the most recent
+    y(k) = x_k.'*c(:,k);
+    e(k) = x(k+N) - y(k); % should predict the next sample
+    c(:,k+1) = c(:,k) + mu*e(k) .* conj(x_k);
+end
+k_c = N-1:K-1;
+k_ey = N-1:K-2;
+end
--- matlab/part4.m
+++ matlab/part4.m
@@ -1,7 +1,7 @@
 close all; clear all; clc;
 
 K = 1000;
-N = 4; % Must be the same one of part3
+N = 2; % Must be the same one of part3
 sigma2w = 0.0002;
 
 load 'rng_seed'
@@ -14,32 +14,46 @@ lambdas = eig(R);
 %mu = 2/(max(lambdas) + min(lambdas)); % ? Max 2/M = 10000 ?
 mu = 0.1;
 fprintf('Setting mu to %f\n', mu);
+fprintf('To be stable it should be under %f\n', 2/max(lambdas));
 
-% Skip the first N-1 samples: we need a full vector x_k
-c = zeros(N, K-N+1);
-y = zeros(K-N+1, 1);
-e = zeros(K-N+1, 1);
-for k=1:K-N % -1 because we need the last d(k)
-    x_k = flip(x(k : k + N - 1)); % The first should be the most recent
-    y(k) = x_k.'*c(:,k);
-    e(k) = x(k+N) - y(k); % should predict the next sample
-    c(:,k+1) = c(:,k) + mu*e(k) .* conj(x_k);
-    %fprintf('deltac:\n');
-    %disp(mu*e(k) .* conj(x_k));
-    %pause;
-    %c(:,k+1) = [1; zeros(N-1,1)];
-end
+[c, e, y, k_c, k_ey] = lsm_predictor(x, N, mu);
 
 fprintf('The value of c at the last iteration is\n');
 disp(c(:,K-N+1).');
 
 figure;
-plot(1:K-N+1, real(c(1,:)));
+subplot(1,2,1);
+plot(k_c, real(c(1,:)));
+xlim([0 200]);
+subplot(1,2,2);
+plot(k_c, imag(c(1,:)));
+xlim([0 200]);
+
+figure;
+subplot(1,2,1);
+plot(k_c, real(c(2,:)));
+xlim([0 200]);
+subplot(1,2,2);
+plot(k_c, imag(c(2,:)));
+xlim([0 200]);
+
+
+% figure;
+% hold on;
+% plot(0:K-1, real(x));
+% plot(N:K-1, real(y));
+
+tries=200;
+es = zeros(K-N, tries);
+for i=1:200
+    [x,k] = generate_x(sigma2w, K, 'shuffle');
+    [~, e_i] = lsm_predictor(x, N, mu);
+    es(:,i) = e_i;
+end
+
+mse = mean(abs(es).^2, 2);
 
 figure;
 hold on;
-plot(0:K-1, real(x));
-plot(N-1:K-1, real(y));
-
-r = theoretical_autocorr(sigma2w, N);
-R = transpose(toeplitz(r));
+plot(k_ey, 10*log10(abs(e).^2));
+plot(k_ey, 10*log10(mse));

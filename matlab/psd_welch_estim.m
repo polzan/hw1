1db54b75b014a9de3684f27add4c70374c186e3e
Riccardo Zanol <ricc@zanol.eu>
2016-04-01 18:37:43 +0200

    Fix the use of Fs in the psd estimation

--- matlab/psd_welch_estim.m
+++ matlab/psd_welch_estim.m
@@ -3,11 +3,10 @@ K = length(x);
 Ndft = length(f);
 Mw = 1/D * (w'*w);
 N_s = floor((K - D) / (D - S) + 1);
-psd_w = zeros(Ndft, N_s);
+psd_chunk = zeros(Ndft, N_s);
 for s = 0:N_s - 1
     x_s = w .* x((s*(D-S) + 1):(s*(D-S) + D));
-    X_s = 1/Fs * fft(x_s, Ndft);
-    psd_w(:,s+1) = 1/(D*Mw) * abs(X_s).^2;
+    psd_chunk(:,s+1) = 1/(D*Mw) * abs(fft(x_s, Ndft)).^2;
 end
-psd = mean(psd_w, 2);
+psd = mean(psd_chunk, 2);
 end

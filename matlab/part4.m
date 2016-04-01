7949a38a767d26970eeb20bcf83eacea57a8f586
Riccardo Zanol <ricc@zanol.eu>
2016-04-01 21:50:02 +0200

    Working LSM predictor

--- matlab/part4.m
+++ matlab/part4.m
@@ -7,19 +7,20 @@ sigma2w = 0.0002;
 load 'rng_seed'
 [x, k] = generate_x(sigma2w, K, rng_seed);
 
-r = theoretical_autocorr(sigma2w, N);
-R = transpose(toeplitz(r));
-lambda = eig(R);
+r_theo = theoretical_autocorr(sigma2w, N-1);
+R = transpose(toeplitz(r_theo));
+lambdas = eig(R);
 
-%mu = 2/(max(lambda) + min(lambda)); % ? Max 2/M = 10000 ?
-%mu = 1e-5;
-mu = 0.25;
-% Skip thefirst N-1 samples: we need a full vector x_k
+%mu = 2/(max(lambdas) + min(lambdas)); % ? Max 2/M = 10000 ?
+mu = 0.1;
+fprintf('Setting mu to %f\n', mu);
+
+% Skip the first N-1 samples: we need a full vector x_k
 c = zeros(N, K-N+1);
 y = zeros(K-N+1, 1);
 e = zeros(K-N+1, 1);
 for k=1:K-N % -1 because we need the last d(k)
-    x_k = x(k : k + N - 1);
+    x_k = flip(x(k : k + N - 1)); % The first should be the most recent
     y(k) = x_k.'*c(:,k);
     e(k) = x(k+N) - y(k); % should predict the next sample
     c(:,k+1) = c(:,k) + mu*e(k) .* conj(x_k);

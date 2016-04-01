9f4b603639e18d4a3aad9526d3a391c7feca1d86
Riccardo Zanol <ricc@zanol.eu>
2016-04-01 18:43:03 +0200

    Use the ar_model function to estimate the filter coeff.s

--- matlab/predictor.m
+++ matlab/predictor.m
@@ -1,9 +1,5 @@
 function [c_opt, f_n, a] = predictor(x, N)
-K = length(x);
-r_whole=autocorrelation_unbiased(x);
-r_n = r_whole(2:N+1);
-R_n = toeplitz(r_whole(1:N)); 
-c_opt = inv(R_n)*r_n;
-a = [1;-c_opt];
+a = ar_model(x, N);
+c_opt = -a(2:N+1);
 f_n = conv(x,a);
 end

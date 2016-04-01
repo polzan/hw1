654e37131f6a829ba5e616863095d503425516fc
Riccardo Zanol <ricc@zanol.eu>
2016-04-01 21:29:41 +0200

    Keep only predictor in part 3

--- matlab/part3.m
+++ matlab/part3.m
@@ -4,54 +4,22 @@ close all; clear all; clc;
 K = 1000;
 sigma2w = 0.0002;
 
-
 load 'rng_seed'
 [x, k] = generate_x(sigma2w, K, rng_seed);
 Fs = 1;
 f = linspace(0, Fs, K);
-% Theoretical psd: F[r(n) = e^j... + e^j... + sigma2*delta] = delta(f - f_1)
-% + delta(f - f_2) + sigma2w
-psd_theor = 2 * sigma2w * ones(K, 1);
-psd_theor(round(f_1 * K)) = 2 * sigma2w + K;
-psd_theor(round(f_2 * K)) = 2 * sigma2w + K;
-
-% Periodogram
-X = fft(x); % Tc = 1
-psd_per = 1/K * abs(X).^2;
-%psd_per_matlab = periodogram(x, rectwin(K), K).';
-
-% Welch
-D = 70;
-S = 20;
-win_welch = blackman(D);
-psd_welch = psd_welch_estim(x, win_welch, D, S);
-
-% Correlogram
-r = autocorrelation_unbiased(x);
-win_corr = blackman(K);
-psd_corr = fft(win_corr .* r);
-
-% AR model
-N = 16;
-psd_ar = psd_ar_estim(x, N);
-%psd_ar_matlab = pcov(x, N, K);
 
 %predictor
-N=4;
+N=2;
 [c, fn, a] = predictor(x,N);
-z = tf2zpk(a,1);
-zplane(z)   %something wrong (not a minimum phase filter)
-
 
 figure;
-hold all;
-plot(f, 10*log10(abs(psd_corr)), 'LineWidth', 1.5);
-plot(f, 10*log10(abs(psd_per)), 'LineWidth', 1.5);
-plot(f, 10*log10(abs(psd_welch)), 'LineWidth', 1.5);
-plot(f, 10*log10(abs(psd_ar)), 'LineWidth', 1.5);
-plot(f, 10*log10(abs(psd_theor)), 'LineWidth', 1.5);
+[H, f] = freqz(1, a, 'whole', 1000, 1);
+subplot(2,1,1);
+plot(f, 20*log10(abs(H)));
+subplot(2,1,2);
+plot(f, angle(H) / pi);
 
-legend('Correlogram', 'Periodogram', 'Welch', 'AR', 'Theoretical');
-ylabel('dB');
-xlabel('Hz');
-%ylim([0 50]);
+figure;
+%[z, p, k] = tf2zpk(1, a);
+zplane(1,a);

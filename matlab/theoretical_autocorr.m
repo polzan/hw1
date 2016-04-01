020ca7c0a04b65b877b2e447f0532d0e7bf7528c
Riccardo Zanol <ricc@zanol.eu>
2016-04-01 18:39:00 +0200

    Fix the theoretical autocorrelation and psd

--- matlab/theoretical_autocorr.m
+++ matlab/theoretical_autocorr.m
@@ -1,9 +1,9 @@
-function [r, n] = theoretical_autocorr(sigma2w, N)
+function r = theoretical_autocorr(sigma2w, N)
 % fixed params
 f_1 = 0.125;
 f_2 = 0.8;
 
-n = transpose(0:N-1);
+n = transpose(0:N);
 r = exp(1j*2*pi*f_1*n) + exp(1j*2*pi*f_2*n);
-r(1) = r(1) + 2*sigma2w;
+r(1) = sigma2w;
 end
--- matlab/theoretical_psd.m
+++ matlab/theoretical_psd.m
@@ -1,11 +1,11 @@
-function [psd, f] = theoretical_psd(sigma2w, f, Fs)
+function psd = theoretical_psd(sigma2w, f, Fs)
 % params
 f_1 = 0.125;
 f_2 = 0.8;
 
 K = length(f);
 F = 1/K;
-psd = 2 * sigma2w * ones(K, 1); % Power of the complex gaussian
+psd = sigma2w * ones(K, 1); % Power of the complex gaussian
 i_1 = round(f_1 / F);
 i_2 = round(f_2 / F);
 psd(i_1) = psd(i_1) + K; % delta as a triangle 2F by K

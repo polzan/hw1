cfc9d84b8ac00adbda97283b4ca91ddd3f49e769
Riccardo Zanol <ricc@zanol.eu>
2016-04-03 23:26:42 +0200

    Add the theoretical values in the plots and print them

--- matlab/part4.m
+++ matlab/part4.m
@@ -7,7 +7,8 @@ sigma2w = 0.0002;
 load 'rng_seed'
 [x, k] = generate_x(sigma2w, K, rng_seed);
 
-r = autocorrelation_unbiased(x, N-1);
+%r = autocorrelation_unbiased(x, N-1);
+r = theoretical_autocorr(sigma2w, N-1);
 R = transpose(toeplitz(r));
 lambdas = eig(R);
 
@@ -21,22 +22,39 @@ fprintf('To be stable it should be under %f (power)\n', 2/(N*r(1)));
 fprintf('The value of c at the last iteration is\n');
 disp(c(:,K-N+1).');
 
+[a, Jmin] = ar_model(x, N, 'theoretical', sigma2w);
+c_opt = -a(2:N+1);
+Jinf = 2/(2 - mu*N*r(1)) * Jmin;
+
 figure;
 subplot(1,2,1);
+hold on;
 plot(k_c, real(c(1,:)));
+plot(0:K-1, real(c_opt(1)) * ones(K, 1), '--');
+ylabel('Real part');
 xlim([0 200]);
 subplot(1,2,2);
+hold on;
 plot(k_c, imag(c(1,:)));
+plot(0:K-1, imag(c_opt(1)) * ones(K, 1), '--');
+ylabel('Imaginary part');
 xlim([0 200]);
+print('coeff_lms_c1', '-depsc');
 
 figure;
 subplot(1,2,1);
+hold on;
 plot(k_c, real(c(2,:)));
+plot(0:K-1, real(c_opt(2)) * ones(K, 1), '--');
+ylabel('Real part');
 xlim([0 200]);
 subplot(1,2,2);
+hold on;
 plot(k_c, imag(c(2,:)));
+plot(0:K-1, imag(c_opt(2)) * ones(K, 1), '--');
+ylabel('Imaginary part');
 xlim([0 200]);
-
+print('coeff_lms_c2', '-depsc');
 
 figure;
 hold on;
@@ -53,8 +71,15 @@ end
 
 mse = mean(abs(es).^2, 2);
 
+
+
 figure;
 hold on;
 plot(k_ey, 10*log10(abs(e).^2));
 plot(k_ey, 10*log10(mse));
+plot(0:K-1, 10*log10(Jmin) * ones(K, 1), '--');
+plot(0:K-1, 10*log10(Jinf) * ones(K, 1), '--');
+legend('|e|^2', 'E[|e|^2]', 'Jmin', 'Jinf');
+ylabel('dB');
 xlim([0 200]);
+print('lms_mse', '-depsc');

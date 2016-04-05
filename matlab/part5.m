932a1f2ede9101171daf9b77ebd8bdbb01115534
Riccardo Zanol <ricc@zanol.eu>
2016-04-05 19:57:48 +0200

    Add part5

--- matlab/part5.m
+++ matlab/part5.m
@@ -1,7 +1,7 @@
 close all; clear all; clc;
 
 K = 1000;
-N = 12; 
+N = 20; 
 sigma2w = 0.0002;
 
 load 'rng_seed_part5'
@@ -11,7 +11,7 @@ D = 5; % Everything > 0 should be fine: the broadband sig. is white
 x_delayed = x(D+1:K);
 k_delayed = D:K-1;
 
-r = theoretical_autocorr(sigma2w, 0);
+r = autocorrelation_unbiased(x_delayed, 0);
 
 mu_tilde = 0.25;
 mu = mu_tilde/(N*r(1)); %the numerator is mu-tilde
@@ -20,18 +20,19 @@ fprintf('Setting mu to %f\n', mu);
 
 [c, e, y, k_c, k_ey] = lms_filter(x(1:K-D+1), x_delayed, N, mu);
 
-figure;
-plot(k_ey, abs(autocorrelation_unbiased(y, length(k_ey)-1)));
+% figure;
+% plot(k_ey, abs(autocorrelation_unbiased(y, length(k_ey)-1)));
+% 
+% figure;
+% plot(k_ey, abs(autocorrelation_unbiased(e, length(k_ey)-1)));
 
-figure;
-plot(k_ey, abs(autocorrelation_unbiased(e, length(k_ey)-1)));
-
-Nar = 12;
-[a, sigma2w] = ar_model(y, Nar);
+Nar = N;
+[a, ~] = ar_model(y, Nar);
 [z,p,~] = tf2zpk(1,a);
 
 figure;
 zplane(z,p);
+print('part5poles', '-depsc');
 
 i_poles = find(abs(abs(p) - 1) <= 1e-3);
 poles = p(i_poles);
